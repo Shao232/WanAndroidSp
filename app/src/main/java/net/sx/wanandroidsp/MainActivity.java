974943486d0a79a83da64856f820930ef88a0f18
1147083532@qq.com <1147083532@qq.com>
2019-02-15 09:02:29 +0800

    first commit

---
+++ app/src/main/java/net/sx/wanandroidsp/MainActivity.java
@@ -0,0 +1,16 @@
+package net.sx.wanandroidsp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * @author ss
+ * created 2019/2/15 8:51
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
